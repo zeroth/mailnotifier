d044892492ca5439bcad65c835f634782b15a396
Abhishek Patil <abhishek@thezeroth.net>
2008-09-21 09:30:25 +0530

    add display the subject header TODO: sort the actual message
    Signed-off-by: Abhishek Patil <abhishek@thezeroth.net>

--- unread.py
+++ unread.py
@@ -16,6 +16,14 @@ def check_unread():
         print "there is/are %d email/s" % len(mailbox.search(None,"UNSEEN")[1][0].split())
     else:
         print " there are no UNREAD Messagees"
+    return mailbox.search(None,"UNSEEN")[1]
+def fetch_mail(mail_uid):
+    for uid in mail_uid[0].split():
+        typ, data = mailbox.fetch(uid,'(BODY[HEADER.FIELDS (FROM TO CC DATE SUBJECT)])')
+        sub = data[0][1].strip()
+        sub = sub.replace('\r\n','\t')
+        print sub
+
 def logout():
     mailbox.logout()
 
@@ -25,7 +33,7 @@ if __name__ == '__main__':
         login(sys.argv[1],sys.argv[2])
     except:
         print "Login Problem : Please try the correct user name and password"
-    check_unread()
+    fetch_mail(check_unread())
     logout()
     
 
